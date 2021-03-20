4b700c4a67c2bd5d7ebea27852ccfbc3275291ce
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2021-03-20 10:25:08 -0400

    feat: initial styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,11 @@ import React from "react"
 import "./layout.css"
 
 export default function Layout({ children }) {
-  return <div>{children}</div>
+  return (
+        <div>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Playfair+Display" />
+            <div className="container">{children}</div>
+        </div>
+    )
 }
 
