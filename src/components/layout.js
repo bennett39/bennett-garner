8dac2360936d0bcba74a066ee51c8573138f8d7c
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2021-03-20 10:35:34 -0400

    feat: start using react helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,16 @@
 import React from "react"
+import {Helmet} from "react-helmet";
 import "./layout.css"
 
 export default function Layout({ children }) {
   return (
-        <div>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Playfair+Display" />
+        <>
+            <Helmet>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Playfair+Display" />
+                <title>Default Title</title>
+            </Helmet>
             <div className="container">{children}</div>
-        </div>
+        </>
     )
 }
 
